49465892e54ef3f72df2c61a05903be523e0f67a
Mike Jose <mikejose.w@gmail.com>
2017-09-30 12:09:59 -0400

    Add project files.

---
+++ KataVM.Library.Tests/VendingMachineTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataVM.Library.Tests
+{
+    [TestFixture]
+    public class VendingMachineTests
+    {
+        [Test]
+        public void WhenWeightEnteredReturnsValue()
+        {
+            VendingMachine vendingMachine = new VendingMachine();
+
+            Assert.AreEqual("$0.05", vendingMachine.GetCoinValue(0.176m));
+        }
+    }
+}
